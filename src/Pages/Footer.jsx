2d84a5bcb12d51318655415b970f3b67d86642e7
Brandon <branstainn@gmail.com>
2023-02-16 00:21:50 -0600

    before animations

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -5,9 +5,9 @@ import footerLogo from '../img/brLogoTraYell.png'
 const Footer = () => {
     return (
         <div className='footer'>
-            <Link to="header" smooth={true} duration={2000}><img className='footerLogo' src={footerLogo} alt="" /></Link> 
-            {/* <Link  className='link' to='contact' >{english ? "Contact" : "Contacto"}</Link> */}
-            <h3 className='footerTxt'>Brandon De La Rosa 2023. All Rights Reserved</h3>
+            {/* <Link to="header" smooth={true} duration={2000}><img className='footerLogo' src={footerLogo} alt="" /></Link>  */}
+            <img className='footerLogo' src={footerLogo} alt="" />
+            <p className='footerTxt'>Brandon De La Rosa 2023. All Rights Reserved</p>
         </div>
     );
 };
